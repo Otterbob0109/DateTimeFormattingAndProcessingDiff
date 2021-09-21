7c626783cc40486fd581ed1b02359dc905f21983
Tom Johnson <59239002+Otterbob0109@users.noreply.github.com>
2021-09-21 07:11:52 -0500

    Initial Commit

---
+++ DateTimesAndProcessingTime/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace DateTimesAndProcessingTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstDateTime = new System.Windows.Forms.ListBox();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstDateTime
+            // 
+            this.lstDateTime.FormattingEnabled = true;
+            this.lstDateTime.ItemHeight = 25;
+            this.lstDateTime.Location = new System.Drawing.Point(12, 12);
+            this.lstDateTime.Name = "lstDateTime";
+            this.lstDateTime.Size = new System.Drawing.Size(627, 379);
+            this.lstDateTime.TabIndex = 0;
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(225, 426);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(214, 34);
+            this.btnDisplay.TabIndex = 1;
+            this.btnDisplay.Text = "Display Date and Time";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 498);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.lstDateTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstDateTime;
+        private System.Windows.Forms.Button btnDisplay;
+    }
+}
+
---
+++ DateTimesAndProcessingTime/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimesAndProcessingTime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            lstDateTime.Items.Clear();
+
+            //Start Date Time Initialization and formatting
+            lstDateTime.Items.Add("Start Date Time Formated several different ways");
+            lstDateTime.Items.Add("----------------------------------------------------");
+            DateTime myDateTime1 = DateTime.Now;
+            lstDateTime.Items.Add("ToString:\t\t" + myDateTime1.ToString());
+            lstDateTime.Items.Add("ToLongTimeString:\t" + myDateTime1.ToLongTimeString());
+            lstDateTime.Items.Add("Millisecond:\t" + myDateTime1.Millisecond);
+            lstDateTime.Items.Add("ToShortDateString:\t" + myDateTime1.ToShortDateString());
+            lstDateTime.Items.Add("ToShortTimeString:\t" + myDateTime1.ToShortTimeString());
+
+            //End Date Time Initialization and formatting
+            lstDateTime.Items.Add("");
+            lstDateTime.Items.Add("End Date Time Formated several different ways");
+            lstDateTime.Items.Add("----------------------------------------------------");
+            DateTime myDateTime2 = DateTime.Now;
+            lstDateTime.Items.Add("ToString:\t\t" + myDateTime2.ToString());
+            lstDateTime.Items.Add("ToLongTimeString:\t" + myDateTime2.ToLongTimeString());
+            lstDateTime.Items.Add("Millisecond:\t" + myDateTime2.Millisecond);
+
+            //Comparison between the Start Time or the End Time
+            lstDateTime.Items.Add("");
+            lstDateTime.Items.Add("End Time - Start Time [hh:mm:ss:ms]");
+            lstDateTime.Items.Add("----------------------------------------------------");
+            lstDateTime.Items.Add("CompareTo:\t" + (myDateTime2 - myDateTime1).ToString());
+
+        }
+    }
+}
